a6c8f975afa639e4500f4fa8c7c49ecab8aa5e49
Caleb Fahlgren <cfahlgren1@gmail.com>
2017-12-07 19:20:08 -0500

    added message boxes and gui

--- QRCodeGenerator.py
+++ QRCodeGenerator.py
@@ -7,12 +7,14 @@ import pyqrcode,png
 from tkinter import *
 from tkinter import filedialog
 from tkinter.filedialog import asksaveasfilename
+import tkinter.messagebox
 from pyqrcode import *
 
 #Build the form
 root = Tk()
 root.title("QR Code Generator")
-root.geometry("600x600")
+root.geometry("600x450")
+root.resizable(0,0)
 
 #Create Text area
 TextArea = Text(width=60, pady=0)
@@ -21,12 +23,26 @@ TextArea.insert(END, "Type url here.")
 #Create Title Label
 titleLabel = Label(text="QR Code", font=("Helvetica", 16))
 
+def create():
+    submit = tkinter.messagebox.askquestion("Submit Entry", "Are you sure you want to submit?")
+    if submit == "yes":
+        try:
+            path = tkinter.filedialog.asksaveasfilename()
+        except Exception:
+            return
+        generateQR(TextArea.get("1.0", END), path)
+
+#Create Button
+createButton = Button(fg="white", bg="green", text="Create", width=50, command=create, activebackground="yellow")
+
 #Take a parameter and generate the QR Code
-def generateQR(url):
-    url = pyqrcode.create("test")
-    url.png('url.png', scale=5)
+def generateQR(url,path):
+    url = pyqrcode.create(url)
+    url.png(path + ".png", scale=5)
+    tkinter.messagebox.showinfo("Created","Created QR Code Successfully!")
 
 #Start GUI
 titleLabel.pack()
 TextArea.pack()
+createButton.pack()
 mainloop()
